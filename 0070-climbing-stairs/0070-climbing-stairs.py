242a1ecc878e735042477cf37f5646857ddb87eb
Siya Sriswal <sriswalsiya@gmail.com>
2026-07-02 21:09:05 +0530

    Time: 1 ms (3.42%), Space: 19.5 MB (16.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,12 @@
+from functools import cache
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [-1] * (n + 1)
-        def f(n,dp):
-            if n<=1:
+
+        @cache
+        def f(n):
+            if n <= 1:
                 return 1
-            if dp[n]!=-1:
-                return dp[n]
-            dp[n]= f(n-1,dp)+f(n-2,dp)
-            return dp[n]
-        return f(n, dp)
+            return f(n-1) + f(n-2)
+
+        return f(n)
